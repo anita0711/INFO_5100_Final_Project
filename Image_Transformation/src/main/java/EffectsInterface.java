c98842389c9808679c90e4ec0fb61408666a6b51
anita0711 <56136387+anita0711@users.noreply.github.com>
2020-01-11 14:48:34 -0800

    INFO 5100 project

---
+++ Image_Transformation/src/main/java/EffectsInterface.java
@@ -0,0 +1,9 @@
+/**
+ * @author Anita Ganani and Qiang Ma
+ */
+public interface EffectsInterface {
+    /**
+     * @param type Method to be implemented by ProjectController to make a choice of effects to be applied on an image.
+     */
+    public void pixWithImage(int type);
+}

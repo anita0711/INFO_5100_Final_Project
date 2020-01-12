aecbe40179e3b96f7551a57857e2f7ebc20631ff
anita0711 <56136387+anita0711@users.noreply.github.com>
2020-01-11 22:56:39 -0800

    Revert "INFO 5100 project"
    This reverts commit c98842389c9808679c90e4ec0fb61408666a6b51.

--- Image_Transformation/src/main/java/EffectsInterface.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Anita Ganani and Qiang Ma
- */
-public interface EffectsInterface {
-    /**
-     * @param type Method to be implemented by ProjectController to make a choice of effects to be applied on an image.
-     */
-    public void pixWithImage(int type);
-}
